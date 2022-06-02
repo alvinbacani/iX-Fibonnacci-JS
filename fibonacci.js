a67e7180ec1ba34270495ca48df4d40e2a1d5f0f
Alvin Bacani <a.bacani@wustl.edu>
2022-06-02 13:23:41 +0200

    Minimized Fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,16 +1,15 @@
 // Alvin Bacani June 1, 2022 Fibonacci Assignment
 
-function fibonacci(current_number, second_number, new_number){
-    current_number = 0
-    second_number = 1
-    new_number = 0
+function fibonacci(current_number, second_number){
 
-    document.write('First 10 Fibonacci Numbers:')
+    document.write('First 10 Fibonacci Numbers: ')
 
-    for (let i = 0; i <= 10; i++){
+    for (let i = 1; i <= 10; i++){
         return document.write(current_number + ", ");
         new_number = current_number + second_number;
         current_number = second_number;
         second_number = new_number;
     }
 }
+
+fibonacci(0, 1);
